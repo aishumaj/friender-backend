ada69c736ded3ecdbb98c4e623b74160b544f068
Aishwarya Majumdar <aishu.maj1996@gmail.com>
2022-09-14 11:21:30 -0700

    uploading to aws function

--- aws_uploads.py
+++ aws_uploads.py
@@ -1,8 +1,49 @@
+import os
+from dotenv import load_dotenv
 import boto3
+from botocore.exceptions import ClientError
+import logging
+load_dotenv()
 
-IMAGE = "/Users/margaret/dev/rithm/Sprints/friender/friender-backend/img/test-pic.jpeg"
+S3_KEY = os.environ.get('S3_KEY')
+S3_SECRET = os.environ.get('S3_SECRET')
+S3_BUCKET = os.environ.get('S3_BUCKET')
+S3_LOCATION = os.environ.get('S3_LOCATION')
 
-s3 = boto3.resource('s3')
-BUCKET = "rithmbucket"
+IMAGE = "img/test_pic.jpeg"
 
-s3.Bucket(BUCKET).upload_file(IMAGE, BUCKET, "test-pic.jpeg")
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def upload_file(file_name, bucket, object_name, acl="public-read"):
+    print("what is file name: ", file_name)
+    #Upload file
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=S3_KEY,
+        aws_secret_access_key=S3_SECRET)
+    
+    try:
+        with open(file_name, 'rb') as data:
+            print("what is data: ", data)
+            response = s3.upload_fileobj(
+                data, 
+                bucket, 
+                object_name,
+                ExtraArgs={
+                    "ACL": acl,
+                    "ContentType": "image/png"
+            })
+            print("response from s3: ", response)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+        
+upload_file(IMAGE, S3_BUCKET,"test")
+
+       
